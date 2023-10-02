26687c5710518346667834c88bea9ead89bfc91e
Eduardo <ealvarez@carminaticonsulting.com>
2023-10-02 12:16:31 -0500

    Adding all new components to git

---
+++ contactopedia/contactopedia/src/Components/ContactPages/GeneralContacts.jsx
@@ -0,0 +1,11 @@
+const GeneralContacts = () => {
+  return (
+    <div>
+      <button className="btn btn-secondary form-control">
+        General Contacts
+      </button>
+    </div>
+  );
+};
+
+export default GeneralContacts;
---
+++ contactopedia/contactopedia/src/Components/ContactPages/RemoveAllContact.jsx
@@ -0,0 +1,9 @@
+const RemoveAllContact = () => {
+  return (
+    <div>
+      <button className="btn btn-danger form-control">Remove all</button>
+    </div>
+  );
+};
+
+export default RemoveAllContact;
