0b43edf665ba525e644ae001829c9c54a619f1bf
Eduardo <ealvarez@carminaticonsulting.com>
2023-10-03 17:06:02 -0500

    Adding icons to button for favorites

--- contactopedia/contactopedia/src/Components/ContactPages/Contact.jsx
+++ contactopedia/contactopedia/src/Components/ContactPages/Contact.jsx
@@ -1,6 +1,34 @@
 const Contact = (props) => {
   return (
-    <div>
+    <div
+      className="row p-md-2 mb-2"
+      style={{ borderRadius: "20px", border: "1px solid #555" }}
+    >
+      <div className="col-2 col-md-1 pt-2 pt-md-1">
+        <img
+          src={`https://ui-avatars.com/api/?name=${props.contact.name}`}
+          style={{ width: "80%" }}
+          alt=""
+        />
+      </div>
+      <div className="col-6 mol-md-5 text-warning pt-0">
+        <span className="h4">{props.contact.name}</span>
+        <br />
+        <div className="text-white-50">
+          {props.contact.email}
+          <br />
+          {props.contact.phone}
+        </div>
+      </div>
+      <div className="col-2 col-md-2 pt-md-3">
+        <button
+          className={`btn btn-sm m-1 ${
+            props.contact.isFavorite ? "btn-warning" : "btn-outline-warning"
+          }`}
+        >
+          <i className="bi bi-star"></i>
+        </button>
+      </div>
       <button className="btn btn-secondary form-control">
         {props.contact.name}
       </button>
